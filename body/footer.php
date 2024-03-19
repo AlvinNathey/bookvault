8c9f67bcceaddba4a1f36e215eb6bf0cd2a9a3fc
AlvinNathey <alvin.nathey@strathmore.edu>
2024-03-19 15:14:56 +0300

    book vault project header and footer done

---
+++ body/footer.php
@@ -0,0 +1,25 @@
+
+</div>
+
+<div class="footer">
+
+    <div class="wrapper">
+
+        <ul>	
+
+            <li><a href="http://twitter.com"><i class="fa-brands fa-x-twitter"></i>Twitter</a></li>
+            <li><a href="https://www.facebook.com"><i class="fa-brands fa-facebook"></i>Facebook</a></li>
+            <li><a href="https://www.instagram.com"><i class="fa-brands fa-instagram"></i>Instagram</a></li>
+            <li><a href="https://www.youtube.com"><i class="fa-brands fa-youtube"></i>YouTube</a></li>
+
+
+        </ul>
+
+        <p>&copy;<?php echo date("Y"); ?> BookVault</p>
+
+    </div>
+
+</div>
+
+</body>
+</html>
